0854f1b402586e9257ba493f36e87c6f1a6790bf
Carmen Morado <cmora171@fiu.edu>
2020-11-24 22:11:17 -0500

    The basic UI has been created using a tab bar controller (in addition to a navigation controller) and the JSON file was parsed using the Codable protocol so that petitions to the White House are displayed in a table view.

---
+++ Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Carmen Morado on 11/24/20.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Carmen Morado on 11/24/20.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
